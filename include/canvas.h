6e911961b7113e3f019930220dc2af5a0eee3491
imaadh-ifthi <imaadh.ifthi@gmail.com>
2025-06-05 13:51:44 +0530

    added canvas.h file, need to fill in some comments tho

---
+++ include/canvas.h
@@ -0,0 +1,21 @@
+#ifndef CANVAS_H //including guard to make sure another canvas_h file is not defined and if so then proceed
+#define CANVAS_H //defining canvas
+
+typedef struct canvas_t  //defining a struct to carry info about the canvas
+{
+    int width;
+    int height;
+    float **pixels;
+};
+
+canvas_t* canvas_create(int width, int height);
+
+void canvas_destroy(canvas_t* canvas);
+
+void set_pixel_f(canvas_t* canvas, float x, float y, float intensity);
+
+void draw_line_f(canvas_t* canvas, float x0, float y0, float x1, float y1, float thickness, float intensity);
+
+int canvas_save_to_pgm(const canvas_t* canvas, const char* filename);
+
+#endif
