878ff1fdca7106861d0fb940767c7e2803e01073
DaniruDC <e22056@eng.pdn.ac.lk>
2025-06-12 00:27:49 +0530

    started on floating point coordinations

---
+++ include/canvas.c
@@ -0,0 +1,11 @@
+#include "canvas.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+void set_pixel_f(canvas_t* canvas, float x, float y, float intensity) {
+    if(!canvas || intensity < 0.0f) return;
+
+
+
+}
